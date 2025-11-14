c2eee9182ca33ef4b5e2553f15ab056f9af266a6
akshay <akshaybachhu16@gmail.com>
2025-11-14 14:45:32 +0000

    Used Automation for pickWinner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -12,8 +12,17 @@ import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFCo
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 error Raffle__SendMoreToEnterRaffle();
+error Raffle__TransferFailed();
+error Raffle__NotOpen();
 
 contract Raffle is VRFConsumerBaseV2Plus {
+    /*Type Declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /* State Variables */
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
     uint256 private immutable i_subscriptionId;
@@ -25,6 +34,11 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint256 private s_requestId;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+    address payable private s_recentWinner;
+    RaffleState private s_raffleState;
+
+    event RaffleEntered(address indexed sender);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -40,20 +54,55 @@ contract Raffle is VRFConsumerBaseV2Plus {
         i_keyHash = keyHash;
         i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
-        s_players.push(payable(msg.sender)); // ETH to winner
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() external {
-        // Check to see if enough time is passed.
-        if (block.timestamp - s_lastTimeStamp < i_interval) {
+    // This is the function that chainlink nodes will call to see if the lottery is
+    // ready to have a winner picked
+    // These things need to be true for upKeepNeeded to be true:
+    // 1.The time interval has passed between raffle runs
+    // 2.The lottery is open
+    // 3. The contract has ETH.
+    // 4. Implicitly, your subscription has LINK.
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool timeHasPassed = block.timestamp - s_lastTimeStamp >= i_interval;
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = timeHasPassed && isOpen && hasBalance && hasPlayers;
+        return (upkeepNeeded, ""); // can also use hex"" instead of "" also null
+    }
+
+    function performUpkeep(
+        bytes calldata /*perform data */
+    )
+        external
+    {
+        (bool upKeepNeeded,) = checkUpkeep("");
+        if (!upKeepNeeded) {
             revert();
         }
+        s_raffleState = RaffleState.CALCULATING;
         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
             keyHash: i_keyHash,
             subId: i_subscriptionId,
@@ -65,35 +114,50 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
+    // CEI: Checks, Effects, Interactions Pattern
     function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        // Checks
+
+        // Effects (Internal Contract State)
         s_randomWord = randomWords[0];
+        uint256 indexOfWinner = randomWords[0] % s_players.length; // 0 to L-1
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        s_players = new address payable[](0);
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(winner);
+
+        // Interactions (External Contract Interactions)
+        (bool success,) = winner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
     }
 
-    /**
-     * Getter Functions
-     */
+    // Getter Functions
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
-
-    // Layout of contracts:
-    // version
-    // imports
-    // errors
-    // interfaces, libraries, contracts
-    // Type declarations
-    // State variables
-    // Events
-    // Modifiers
-    // Functions
-
-    //Layout of Functions:
-    // constructor
-    // receive function
-    // fallback
-    // external
-    // public
-    // internal
-    // private
-    // view & pure functions
 }
+// Layout of contracts:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+//Layout of Functions:
+// constructor
+// receive function
+// fallback
+// external
+// public
+// internal
+// private
+// view & pure functions
+
